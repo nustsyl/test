355195e9d7c68b83af6a6d75afbbf858b4e2ec4b
nust_leaky <nust_leaky@aliyun.com>
2018-09-11 23:00:11 +0800

    第1次
    .........

--- git_test/src/test_1/demo_1.java
+++ git_test/src/test_1/demo_1.java
@@ -6,11 +6,14 @@ public class demo_1
 	{
 		System.out.println("hello git");
 		
+<<<<<<< HEAD
 		System.out.println("你好 git");
 		
 		System.out.println("你好 git，我是xxx");
 		System.out.println("你好 git，我是xxx");
+=======
+		System.out.println("��� git");
+>>>>>>> parent of 5f1c47c... 第五次提交
 	}
 
 }
-  
---
+++ syl_test/src/syl_test/demo_11.java
@@ -0,0 +1,18 @@
+package syl_test;
+
+public class demo_11
+{
+	
+		public static void main(String[] args)
+		{
+			System.out.println("hello git");
+			
+
+			System.out.println("你好 git");
+			
+			System.out.println("你好 git，我是xxx");
+
+		}
+
+}
+
